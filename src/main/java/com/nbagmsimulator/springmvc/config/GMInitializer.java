e967d0a2937c0ad5b466f8f2befc61860be6255c
Kumar <ashwin.q.kumar@afs.com>
2023-10-25 13:48:23 -0400

    add initializer to config

---
+++ src/main/java/com/nbagmsimulator/springmvc/config/GMInitializer.java
@@ -0,0 +1,31 @@
+package com.nbagmsimulator.springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class GMInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {GMConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+	@Override
+    protected Filter[] getServletFilters() {
+        Filter [] singleton = { new CORSFilter() };
+        return singleton;
+    }
+
+}
