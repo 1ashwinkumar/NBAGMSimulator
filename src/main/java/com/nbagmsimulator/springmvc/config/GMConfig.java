8c66054599d264a2c94ddcd78a507239b391baee
Kumar <ashwin.q.kumar@afs.com>
2023-10-25 13:40:45 -0400

    refactor config

--- src/main/java/com/nbagmsimulator/springmvc/config/GMConfig.java
+++ src/main/java/com/nbagmsimulator/springmvc/config/GMConfig.java
@@ -1,5 +1,29 @@
 package com.nbagmsimulator.springmvc.config;
 
-public class GMConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.nbagmsimulator.springmvc")
+@EnableWebMvc
+public class GMConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/");
+		resolver.setSuffix(".jsp");
+		registry.viewResolver(resolver);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
 
 }
